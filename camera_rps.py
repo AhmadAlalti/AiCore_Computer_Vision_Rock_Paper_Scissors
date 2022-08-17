c46dc71fa1edd7edc97f25b1ac9fe60f0dd0496b
Ahmad <110533174+alalti@users.noreply.github.com>
2022-08-17 22:54:32 +0100

    Edits better structure but same output.

--- camera_rps.py
+++ camera_rps.py
@@ -19,41 +19,28 @@ class RPS:
             print(f'{countdown}')
             cv2.waitKey(1000)
             countdown -= 1
-        print('\nShow your hand now')
-    
-    # def count_waiting(self):
-    #     counter = 2
-    #     while counter > 0:
-    #         cv2.waitKey(1000)
-    #         print("---Waiting---")
-    #         counter -= 1        
+        print('\nShow your hand now')     
 
-    def get_user_choice(self):
-        end = time.time() + 3
+    def get_prediction(self):
+        end = time.time() + 1
         while time.time() < end:
             ret, frame = self.cap.read()
             resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
             image_np = np.array(resized_frame)
             normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
             cv2.imshow('frame', frame)
+            prediction = self.model.predict(self.data)
+            choice = choices_list[prediction.argmax()]
+            print(f"You chose {choice}")
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-        return normalized_image        
-
-    def get_prediction(self):
-        prediction = self.model.predict(self.data)
-        choice = choices_list[prediction.argmax()]
-        print(f"You chose {choice}")
-        return choice
+        return choice        
 
     def get_computer_choice(self):
         computer_choice = random.choice(choices_list)
         return computer_choice
 
     def get_winner(self, choice, computer_choice):
-        choice = self.get_prediction
-        computer_choice = self.get_computer_choice
-        self.count_waiting
         if choice == computer_choice:
             print(f"Draw! The computer chose {computer_choice}. Same as you!")
 
@@ -81,7 +68,6 @@ class RPS:
 def play_game(choices_list):
     game = RPS()
     game.count_countdown()
-    game.get_user_choice()
     user_choice = game.get_prediction()
     computer_choice = game.get_computer_choice()
     game.get_winner(user_choice, computer_choice)
@@ -89,8 +75,9 @@ def play_game(choices_list):
 # After the loop release the cap object
     game.cap.release()
 # Destroy all the windows
-    game.cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     choices_list = ['rock', 'paper', 'scissors', 'nothing']
-    play_game()
+    play_game(choices_list)
+
