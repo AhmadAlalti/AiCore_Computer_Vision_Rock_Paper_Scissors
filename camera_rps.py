2d4726eeeef73c1ed33d133de4b67faeac4fdb68
Ahmad <110533174+alalti@users.noreply.github.com>
2022-08-22 18:57:39 +0100

    edits final pieces of code

--- camera_rps.py
+++ camera_rps.py
@@ -33,19 +33,19 @@ class RPS:
             self.data[0] = normalized_image
             cv2.imshow('frame', frame)
             prediction = self.model.predict(self.data)
-            print(prediction)
             self.choice = choices_list[prediction.argmax()]
-            print(f"You chose {self.choice}")
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         return self.choice        
+        
+        
 
     def get_computer_choice(self):
-        computer_choice = random.choice(choices_list)
+        computer_choice = random.choice(choices_list[:-1])
         return computer_choice
 
     def get_winner(self, choice, computer_choice):
-        if self.choice == "nothing":
+        if choice == "nothing":
             print("Restart, and choose something this time!")
 
         elif choice == computer_choice:
@@ -54,10 +54,11 @@ class RPS:
         elif (choice == "rock" and computer_choice == "paper") or \
             (choice == "paper" and computer_choice == "scissors") or \
             (choice == "scissors" and computer_choice == "rock"):
-                print(f"You lost! the computer picked {computer_choice}")
+                print(f"You lost! the computer picked {computer_choice} while you picked {choice}")
                 self.computer_wins += 1
 
         else:
+            print(f"You won! the computer picked {computer_choice} while you picked {choice}")
             self.user_wins += 1
 
         
